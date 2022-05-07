d2f4c9f9226190ad130f86aa2cb3de63f0140610
Anindya Mitra <anindya15l12@gmail.com>
2022-05-07 14:30:28 +0530

    webpage hosted from SPIFFS successfully

--- power-iot-meter-hw/src/main.cpp
+++ power-iot-meter-hw/src/main.cpp
@@ -2,26 +2,56 @@
 #include <SPI.h>
 #include <SPIFFS.h>
 #include "fileOperation.h"
-
-
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+AsyncWebServer server(80);
 
 void setup()
 {
   Serial.begin(115200);
-  // initFlashFileSystem(SPIFFS);
-  // listDir(SPIFFS);
-  // SPIFFS.end();
+  initFlashFileSystem(SPIFFS);
 
-  // WiFi.mode(WIFI_STA);
-  // WiFi.begin("AKM_MACHINE_ULTRA", "zaq12wsx");
-  // while (WiFi.status() != WL_CONNECTED)
-  // {
-  //   Serial.println('.');
-  //   delay(1000);
-  // }
-  // Serial.println(WiFi.localIP());
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("AKM_MACHINE_ULTRA", "zaq12wsx");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.println('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+  server.serveStatic("/", SPIFFS, "/");
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *req)
+            { req->send(SPIFFS, "/index.html", "text/html"); });
 
+  server.on("/scan",
+            HTTP_GET,
+            [](AsyncWebServerRequest *req)
+            {
+              int n = WiFi.scanNetworks();
+              if (!n)
+              {
+                req->send(200, "text/plain", n + " network(s) found");
+              }
+              String dump = "";
+              for (int i = 0; i < n; ++i)
+              {
+                // Print SSID and RSSI for each network found
+                // Serial.print(i + 1);
+                // Serial.print(": ");
+                dump += WiFi.SSID(i);
+                dump += " ";
+                dump += WiFi.RSSI(i);
+                dump += " ";
+                dump += (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*";
+                dump += "\n";
+              }
+              req->send(200, "text/plain", dump);
+              dump.clear();
+            });
 
+  server.begin();
+  listDir(SPIFFS);
 }
 
 void loop()
