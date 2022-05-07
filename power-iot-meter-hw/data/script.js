bcd6c15fc44abec4a722141e3e5072b8514e5257
Anindya Mitra <anindya15l12@gmail.com>
2022-05-07 21:27:43 +0530

    pushed changes so far

--- power-iot-meter-hw/data/script.js
+++ power-iot-meter-hw/data/script.js
@@ -3,6 +3,7 @@ const scanAps = () => {
   xhttp.onreadystatechange =()=>{
     if (this.readyState == 4 && this.status == 200) {
       //apppend the list to the document with id = "scan"
+      console.log(this.Response);
     }
     xhttp.open("GET", "ajax_info.txt", true);
     xhttp.send();
--- power-iot-meter-hw/src/main.cpp
+++ power-iot-meter-hw/src/main.cpp
@@ -5,7 +5,12 @@
 #include <WiFi.h>
 #include <AsyncTCP.h>
 #include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+
 AsyncWebServer server(80);
+bool to_scan;
+int n = 0;
+String scanResults;
 
 void setup()
 {
@@ -21,39 +26,56 @@ void setup()
   }
   Serial.println(WiFi.localIP());
   server.serveStatic("/", SPIFFS, "/");
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *req)
-            { req->send(SPIFFS, "/index.html", "text/html"); });
+  server
+      .on(
+          "/",
+          HTTP_GET,
+          [](AsyncWebServerRequest *req)
+          { to_scan = true;
+              req->send(SPIFFS, "/index.html", "text/html"); });
 
-  server.on("/scan",
-            HTTP_GET,
-            [](AsyncWebServerRequest *req)
+  server
+      .on(
+          "/scan",
+          HTTP_GET,
+          [](AsyncWebServerRequest *req)
+          {
+            if (!n)
             {
-              int n = WiFi.scanNetworks();
-              if (!n)
-              {
-                req->send(200, "text/plain", n + " network(s) found");
-              }
-              String dump = "";
-              for (int i = 0; i < n; ++i)
-              {
-                // Print SSID and RSSI for each network found
-                // Serial.print(i + 1);
-                // Serial.print(": ");
-                dump += WiFi.SSID(i);
-                dump += " ";
-                dump += WiFi.RSSI(i);
-                dump += " ";
-                dump += (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*";
-                dump += "\n";
-              }
-              req->send(200, "text/plain", dump);
-              dump.clear();
-            });
+              req->send(200, "text/plain", n + " network(s) found");
+            }
+            req->send(200, "text/plain", scanResults);
+          });
 
   server.begin();
+
   listDir(SPIFFS);
 }
 
 void loop()
 {
+  if (to_scan)
+  {
+    n = WiFi.scanNetworks();
+    if (!n)
+    {
+      return;
+    }
+    scanResults.clear();
+
+    for (int i = 0; i < n; ++i)
+    {
+      // Print SSID and RSSI for each network found
+      scanResults += String(i + 1);
+      scanResults += " ";
+      // Serial.print(": ");
+      scanResults += WiFi.SSID(i);
+      scanResults += " ";
+      scanResults += WiFi.RSSI(i);
+      scanResults += " ";
+      scanResults += (WiFi.encryptionType(i) == WIFI_AUTH_OPEN) ? " " : "*";
+      scanResults += "\n";
+    }
+    to_scan = false;
+  }
 }
