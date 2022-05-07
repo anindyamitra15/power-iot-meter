7146da63c328f068434ba72b141e9d8335de9879
Anindya Mitra <anindya15l12@gmail.com>
2022-05-07 14:06:45 +0530

    added webpages for wifimanagement

---
+++ power-iot-meter-hw/data/script.js
@@ -0,0 +1,12 @@
+const scanAps = () => {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange =()=>{
+    if (this.readyState == 4 && this.status == 200) {
+      //apppend the list to the document with id = "scan"
+    }
+    xhttp.open("GET", "ajax_info.txt", true);
+    xhttp.send();
+  };
+};
+
+$(document).ready(() => {});
--- power-iot-meter-hw/src/main.cpp
+++ power-iot-meter-hw/src/main.cpp
@@ -4,12 +4,24 @@
 #include "fileOperation.h"
 
 
+
 void setup()
 {
   Serial.begin(115200);
-  initFlashFileSystem(SPIFFS);
-  listDir(SPIFFS);
-  SPIFFS.end();
+  // initFlashFileSystem(SPIFFS);
+  // listDir(SPIFFS);
+  // SPIFFS.end();
+
+  // WiFi.mode(WIFI_STA);
+  // WiFi.begin("AKM_MACHINE_ULTRA", "zaq12wsx");
+  // while (WiFi.status() != WL_CONNECTED)
+  // {
+  //   Serial.println('.');
+  //   delay(1000);
+  // }
+  // Serial.println(WiFi.localIP());
+
+
 }
 
 void loop()
