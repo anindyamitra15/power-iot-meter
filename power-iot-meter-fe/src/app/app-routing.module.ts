08c7a75e46813d733a01ad6b8e6bf3f4a50d9207
Anindya Mitra <anindya15l12@gmail.com>
2022-06-03 22:07:00 +0530

    major changes: implemented firebase and firebase-auth

--- power-iot-meter-fe/src/app/app-routing.module.ts
+++ power-iot-meter-fe/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { MainComponent } from './layout/main/main.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: MainComponent,
+  children: [
+    {
+      path: '',
+      component: DashboardComponent
+    }
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- power-iot-meter-fe/src/environments/environment.ts
+++ power-iot-meter-fe/src/environments/environment.ts
@@ -3,6 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'power-iot-meter',
+    appId: '1:448279949784:web:ff970e5bbd182fe89782ad',
+    databaseURL: 'https://power-iot-meter-default-rtdb.asia-southeast1.firebasedatabase.app',
+    storageBucket: 'power-iot-meter.appspot.com',
+    apiKey: 'AIzaSyA2ZWrZjc6WUU3f4L5bRcnbwtD-Yaf45Cw',
+    authDomain: 'power-iot-meter.firebaseapp.com',
+    messagingSenderId: '448279949784',
+    measurementId: 'G-9WYCNGL8GL',
+  },
   production: false
 };
 
